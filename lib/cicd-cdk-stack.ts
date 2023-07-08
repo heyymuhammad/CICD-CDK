9236f4462ee346e9529054f181ef0d8615a707e0
heyymuhammad <muawan541@gmail.com>
2023-07-08 17:30:14 +0500

    new

--- lib/cicd-cdk-stack.ts
+++ lib/cicd-cdk-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+// import { MyPipelineAppStage } from './stage';
 
-export class CicdCdkStack extends cdk.Stack {
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CicdCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('heyymuhammad/CICD-CDK', 'main'), //Remember to change 
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      })
+    });
   }
 }
